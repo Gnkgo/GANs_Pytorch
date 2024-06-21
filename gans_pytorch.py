f03f482bd4b67f2ac0c7b29c0e93d3b0e41faab3
gnkgo <joanna.brodbeck@hotmail.ch>
2024-06-21 23:32:33 +0200

    update

---
+++ gans_pytorch.py
@@ -0,0 +1,362 @@
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+import torch.nn as nn
+
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+from torch.nn import Module, Sequential, Conv2d, ConvTranspose2d, LeakyReLU, BatchNorm2d, ReLU, Tanh, Sigmoid, BCELoss 
+import os
+
+from numpy import asarray
+from numpy import savez_compressed
+from PIL import Image
+
+
+# path to the image directory
+dir_data  = "segmentation_output"
+ 
+# setting image shape to 32x32
+img_shape = (32, 32, 3)
+ 
+# listing out all file names
+nm_imgs   = np.sort(os.listdir(dir_data))
+X_train = []
+for file in nm_imgs:
+    try:
+        img = Image.open(dir_data+'/'+file)
+        img = img.convert('RGB')
+        img = img.resize((32,32))
+        img = np.asarray(img)/255
+        X_train.append(img)
+    except:
+        print("something went wrong")
+ 
+X_train = np.array(X_train)
+X_train.shape
+savez_compressed('kaggle_images_32x32.npz', X_train)
+# load dict of arrays
+dict_data = np.load('kaggle_images_32x32.npz')
+ 
+# extract the first array
+data = dict_data['arr_0']
+ 
+# print the array
+print(data)
+# Always good to check if gpu support available or not
+dev = 'cuda:0' if torch.cuda.is_available() == True else 'cpu'
+device = torch.device(dev)
+
+# plot images in a nxn grid
+
+def save_images(imgs, grid_size=5, epoch=0, iteration=0):
+    """
+    imgs: vector containing all the numpy images
+    grid_size: 2x2 or 5x5 grid containing images
+    epoch: current epoch number
+    iteration: current iteration number
+    """
+    fig = plt.figure(figsize=(8, 8))
+    columns = rows = grid_size
+    plt.title("Training Images")
+    for i in range(1, columns*rows + 1):
+        plt.axis("off")
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(imgs[i])
+    plt.savefig(f'generated_images/generated_images_epoch_{epoch}_iter_{iteration}.png')
+    plt.close(fig)
+
+def plot_images(imgs, grid_size = 5):
+    """
+    imgs: vector containing all the numpy images
+    grid_size: 2x2 or 5x5 grid containing images
+    """
+    
+    fig = plt.figure(figsize = (8, 8))
+    columns = rows = grid_size
+    plt.title("Training Images")
+
+    for i in range(1, columns*rows +1):
+        plt.axis("off")
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(imgs[i])
+    plt.show()
+# load the numpy vector containing image representations
+imgs = np.load('kaggle_images_32x32.npz')
+
+# to check all the files contained in it
+imgs.files
+
+# this is where all our images are saved
+imgs['arr_0'].shape
+
+# pls ignore the poor quality of the images as we are working with 32x32 sized images.
+plot_images(imgs['arr_0'], 3)
+
+save_images(imgs['arr_0'], 3)
+# Always good to check if gpu support available or not
+
+dev = 'cuda:0' if torch.cuda.is_available() == True else 'cpu'
+device = torch.device(dev)
+
+os.makedirs('generated_images', exist_ok=True)
+
+# To check the device name
+print ('Current cuda device name ', torch.cuda.get_device_name())
+# Preparing custom dataset class - https://pytorch.org/tutorials/beginner/data_loading_tutorial.html#dataset-class
+
+
+class HumanFacesDataset(Dataset):
+    """Human Faces dataset."""
+
+    def __init__(self, npz_imgs):
+        """
+        Args:
+            npz_imgs (string): npz file with all the images (created in gan.ipynb)
+        """
+        self.imgs = npz_imgs
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        image = self.imgs[idx]
+
+        return image
+imgs['arr_0'][0].dtype # it will output float 64 i.e. double
+
+# must convert it to float 32 (which is same as model weights)
+np.float32(imgs['arr_0']).dtype
+
+# Preparing dataloader for training
+
+transpose_imgs = np.transpose( # imp step to convert image size from (7312, 32,32,3) to (7312, 3,32,32)
+    np.float32(imgs['arr_0']), # imp step to convert double -> float (by default numpy input uses double as data type)
+    (0, 3,1,2) # tuple to describe how to rearrange the dimensions
+    ) 
+
+dset = HumanFacesDataset(transpose_imgs) # passing the npz variable to the constructor class
+batch_size = 32
+shuffle = True
+
+dataloader = DataLoader(dataset = dset, batch_size = batch_size, shuffle = shuffle)
+
+# Defining the Generator class
+
+class Generator(Module):
+    def __init__(self):
+
+        # calling constructor of parent class
+        super().__init__()
+
+        self.gen = Sequential(
+          ConvTranspose2d(in_channels = 100, out_channels = 512 , kernel_size = 4, stride = 1, padding = 0, bias = False),
+          # the output from the above will be b_size ,512, 4,4
+          BatchNorm2d(num_features = 512), # From an input of size (b_size, C, H, W), pick num_features = C
+          ReLU(inplace = True),
+
+          ConvTranspose2d(in_channels = 512, out_channels = 256 , kernel_size = 4, stride = 2, padding = 1, bias = False),
+          # the output from the above will be b_size ,256, 8,8
+          BatchNorm2d(num_features = 256),
+          ReLU(inplace = True),
+
+          ConvTranspose2d(in_channels = 256, out_channels = 128 , kernel_size = 4, stride = 2, padding = 1, bias = False),
+          # the output from the above will be b_size ,128, 16,16
+          BatchNorm2d(num_features = 128),
+          ReLU(inplace = True),
+
+          ConvTranspose2d(in_channels = 128, out_channels = 3 , kernel_size = 4, stride = 2, padding = 1, bias = False),
+          # the output from the above will be b_size ,3, 32,32
+          Tanh()
+        
+        )
+
+    def forward(self, input):
+        return self.gen(input)
+
+
+
+netG = Generator().to(device)
+
+print(netG)
+# As an example, to show the shape of the output from the generator -  must be an image
+t = torch.randn(2, 100, 1, 1)
+netG(t.to(device)).shape
+def init_weights(m):
+    if type(m) == ConvTranspose2d:
+        nn.init.normal_(m.weight, 0.0, 0.02)
+    elif type(m) == BatchNorm2d:
+        nn.init.normal_(m.weight, 1.0, 0.02)
+        nn.init.constant_(m.bias, 0)
+# Let's initialize the weights randomly (for a single layer)
+netG.apply(init_weights)
+
+# Print the model
+print(netG)
+# Defining the Discriminator class
+
+# Save function
+def save_model(epoch, generator, discriminator, optimizer_g, optimizer_d, file_path='model.pth'):
+    torch.save({
+        'epoch': epoch,
+        'generator_state_dict': generator.state_dict(),
+        'discriminator_state_dict': discriminator.state_dict(),
+        'optimizer_g_state_dict': optimizer_g.state_dict(),
+        'optimizer_d_state_dict': optimizer_d.state_dict(),
+    }, file_path)
+    print(f"Model saved to {file_path}")
+    
+    # Load model example
+def load_model(file_path, generator, discriminator, optimizer_g, optimizer_d):
+    checkpoint = torch.load(file_path)
+    generator.load_state_dict(checkpoint['generator_state_dict'])
+    discriminator.load_state_dict(checkpoint['discriminator_state_dict'])
+    optimizer_g.load_state_dict(checkpoint['optimizer_g_state_dict'])
+    optimizer_d.load_state_dict(checkpoint['optimizer_d_state_dict'])
+    epoch = checkpoint['epoch']
+    print(f"Model loaded from {file_path}, epoch {epoch}")
+    return epoch
+
+class Discriminator(Module):
+    def __init__(self):
+
+        super().__init__()
+        self.dis = Sequential(
+
+            # input is (3, 32, 32)
+            Conv2d(in_channels = 3, out_channels = 32, kernel_size = 4, stride = 2, padding = 1, bias=False),
+            # ouput from above layer is b_size, 32, 16, 16
+            LeakyReLU(0.2, inplace=True),
+
+            Conv2d(in_channels = 32, out_channels = 32*2, kernel_size = 4, stride = 2, padding = 1, bias=False),
+            # ouput from above layer is b_size, 32*2, 8, 8
+            BatchNorm2d(32 * 2),
+            LeakyReLU(0.2, inplace=True),
+
+            Conv2d(in_channels = 32*2, out_channels = 32*4, kernel_size = 4, stride = 2, padding = 1, bias=False),
+            # ouput from above layer is b_size, 32*4, 4, 4
+            BatchNorm2d(32 * 4),
+            LeakyReLU(0.2, inplace=True),
+
+            Conv2d(in_channels = 32*4, out_channels = 32*8, kernel_size = 4, stride = 2, padding = 1, bias=False),
+            # ouput from above layer is b_size, 256, 2, 2
+            # NOTE: spatial size of this layer is 2x2, hence in the final layer, the kernel size must be 2 instead (or smaller than) 4
+            BatchNorm2d(32 * 8),
+            LeakyReLU(0.2, inplace=True),
+
+            Conv2d(in_channels = 32*8, out_channels = 1, kernel_size = 2, stride = 2, padding = 0, bias=False),
+            # ouput from above layer is b_size, 1, 1, 1
+            Sigmoid()
+        )
+    
+    def forward(self, input):
+        return self.dis(input)
+# As an example, to show the shape of the output from the generator -  must be an integer
+
+t = torch.randn(2, 3, 32, 32)
+
+netD = Discriminator().to(device)
+netD(t.to(device)).shape
+# initializing the weights
+netD.apply(init_weights)
+
+print(netD)
+# Setting up otimizers for both Generator and Discriminator
+
+opt_D = optim.Adam(netD.parameters(), lr = 0.0002, betas= (0.5, 0.999))
+opt_G = optim.Adam(netG.parameters(), lr = 0.0002, betas= (0.5, 0.999))
+
+# Setting up the loss function - BCELoss (to check how far the predicted value is from real value)
+
+loss = BCELoss()
+# TRAINING GANS
+epochs = 1000
+
+# going over the entire dataset 10 times
+for e in range(epochs):
+    
+    # pick each batch b of input images: shape of each batch is (32, 3, 32, 32)
+    for i, b in enumerate(dataloader):
+
+        ##########################
+        ## Update Discriminator ##
+        ##########################
+
+        # Loss on real images
+        
+        # clear the gradient
+        opt_D.zero_grad() # set the gradients to 0 at start of each loop because gradients are accumulated on subsequent backward passes
+        # compute the D model output
+        yhat = netD(b.to(device)).view(-1) # view(-1) reshapes a 4-d tensor of shape (2,1,1,1) to 1-d tensor with 2 values only
+        # specify target labels or true labels
+        target = torch.ones(len(b), dtype=torch.float, device=device)
+        # calculate loss
+        loss_real = loss(yhat, target)
+        # calculate gradients -  or rather accumulation of gradients on loss tensor
+        loss_real.backward()
+
+        # Loss on fake images
+
+        # generate batch of fake images using G
+        # Step1: creating noise to be fed as input to G
+        noise = torch.randn(len(b), 100, 1, 1, device = device)
+        # Step 2: feed noise to G to create a fake img (this will be reused when updating G)
+        fake_img = netG(noise) 
+
+        # compute D model output on fake images
+        yhat = netD.cuda()(fake_img.detach()).view(-1) # .cuda() is essential because our input i.e. fake_img is on gpu but model isnt (runtimeError thrown); detach is imp: Basically, only track steps on your generator optimizer when training the generator, NOT the discriminator. 
+        # specify target labels
+        target = torch.zeros(len(b), dtype=torch.float, device=device)
+        # calculate loss
+        loss_fake = loss(yhat, target)
+        # calculate gradients
+        loss_fake.backward()
+
+        # total error on D
+        loss_disc = loss_real + loss_fake
+
+        # Update weights of D
+        opt_D.step()
+
+        ##########################
+        #### Update Generator ####
+        ##########################
+
+        # clear gradient
+        opt_G.zero_grad()
+        # pass fake image through D
+        yhat = netD.cuda()(fake_img).view(-1)
+        # specify target variables - remember G wants D *to think* these are real images so label is 1
+        target = torch.ones(len(b), dtype=torch.float, device=device)
+        # calculate loss
+        loss_gen = loss(yhat, target)
+        # calculate gradients
+        loss_gen.backward()
+        # update weights on G
+        opt_G.step()
+
+
+        ####################################
+        #### Plot some Generator images ####
+        ####################################
+
+        # during every epoch, print images at every 10th iteration.
+        if i% 100 == 0:
+            # convert the fake images from (b_size, 3, 32, 32) to (b_size, 32, 32, 3) for plotting 
+            img_plot = np.transpose(fake_img.detach().cpu(), (0,2,3,1)) # .detach().cpu() is imp for copying fake_img tensor to host memory first
+            plot_images(img_plot)
+            save_images(img_plot, grid_size=5, epoch=e, iteration=i)
+            print("********************")
+            print(" Epoch %d and iteration %d " % (e, i))
+       
+    if e % 100 == 0 :
+        save_model(e, netG, netD, opt_G, opt_D, file_path=f'model_epoch_{e}.pth')
+
+        
+        
